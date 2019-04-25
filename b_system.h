66a299d85da1187eda660cd55c65bdd5ddb24700
Peter <42987392+Gpeter28@users.noreply.github.com>
2019-04-25 21:45:36 +0800

    Create b_system.h

---
+++ b_system.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+#define LEN sizeof(struct Book)
+
+typedef struct Book
+{
+	char num[14];
+	char title[21];
+	char author[9];
+	char publisher[21];
+	char time[11];
+	char price[7];
+	struct Book *next;
+}Book;
+
+Book stu, tem;
+
+FILE *fp;
+struct Book *p1, *p2, *p3, *head, *tail;
+typedef unsigned char SString[30];
+
